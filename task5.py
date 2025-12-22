4d6d5d3b96369148d04f8b11d1bb67be70129447
CrowleyLovelet <stillalivecrowley@gmail.com>
2025-12-22 19:32:50 +0300

    Delete task5.py

--- task5.py
+++
@@ -1,7 +0,0 @@
-r=int(input("radius"))
-x=int(input("x for a dot"))
-y=int(input("y for a dot"))
-if x**2 + y**2 > r**2:
-    print("dot is not inside a circle")
-else:
-    print("dot is inside a circle")
